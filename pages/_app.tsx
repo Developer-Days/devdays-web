8122b1a81e8475a63c0d1e278bc915d4712991e5
dewanshDT <dewansh.dt@gmail.com>
2022-11-24 12:31:26 +0530

    font and dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { Almarai, League_Spartan } from "@next/font/google"
+
+const almarai = Almarai({
+  weight: "400",
+  style: "normal",
+  subsets: ["arabic"],
+  variable: "--font-almarai",
+})
+
+const spartan = League_Spartan({
+  weight: "500",
+  style: "normal",
+  variable: "--font-spartan",
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${almarai.variable} ${spartan.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+// const { fontFamily } = require("tailwindcss/defaultTheme")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "accent-blue": "#001970",
+        "accent-50": "#F4F4F4",
+        "accent-orange": "#FF5D27",
+        "accent-orange-100": "#EF514021",
+      },
+    },
+    listStyleType: {
+      none: "none",
+      disc: "disc",
+      decimal: "decimal",
+      square: "square",
+      roman: "upper-roman",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      "League-Spartan": ["var(--font-spartan)"],
+      almarai: ["var(--font-almarai)"],
+    },
+  },
+  // plugins: [require("@tailwindcss/line-clamp")],
+}
