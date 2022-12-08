2f3f44a6dc8c1aff75e80e80a0c3d3eefb7ddf6e
Rahul Kesharwani <rahulkesharwani353@gmail.com>
2022-12-08 10:09:07 +0530

    favicon added and meta data added

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,12 @@ export default function Home() {
   return (
     <div className="w-full container flex flex-col">
       <Head>
-        <title>Developer Days</title>
+        <meta
+      name="description"
+      content="Developer Days is a 5-week long event. It consists of three tracks that are Web Development, Android Development, and UI/UX Design. The event consists of various sessions and challenges. Various swags and prizes will be awarded to the winners and participants"
+    />
+    <meta name="keywords" content="Developer Days, DevDays, bootcamp, hackathon, DeveloperDays, Developer Days 2021"/>
+    <title>Developer Days</title>
         <meta name="description" content="Developer days is coming back!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -26,9 +31,15 @@ export default function Home() {
               <h5 className="font-League-Spartan">
                 Want to have a look at Developer Days 1.0?
               </h5>
+              <a
+              target={"_blank"}
+              rel={"noreferrer"}
+              href="https://2021.developerdays.tech/"
+            >
               <button className="px-4 hover:brightness-105 rounded-xl py-2 font-League-Spartan text-2xl text-white bg-[#1EB7D0]">
                 Have a look
               </button>
+              </a>
             </div>
           </div>
           <div className="flex w-full items-center justify-end">
